d034fd8136b7bf1d48b0d74eba49c6397000b30a
Marvin Estrada <marvinestrada2@gmail.com>
2017-08-31 11:38:10 -0600

    .

--- Proyecto_EDD/src/proyecto_edd/Proyecto_EDD.java
+++ Proyecto_EDD/src/proyecto_edd/Proyecto_EDD.java
@@ -1,24 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto_edd;
 
 import java.util.Scanner;
 
-
-/**
- *
- * @author Usuario Dell
- */
 public class Proyecto_EDD {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         TDA_Arbol ta = new TDA_Arbol();
         
--- Proyecto_EDD/src/proyecto_edd/TDA_Arbol.java
+++ Proyecto_EDD/src/proyecto_edd/TDA_Arbol.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto_edd;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Usuario Dell
- */
 public class TDA_Arbol {
 
     private ArrayList<Arbol> listaarbol = new ArrayList();
