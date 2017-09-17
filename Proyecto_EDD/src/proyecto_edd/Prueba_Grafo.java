a9006a7376714706256cfa1e6781fcd5d1beff96
Marvin Estrada <marvinestrada2@gmail.com>
2017-09-17 15:26:55 -0600

    Errores de Grafos Bi

--- Proyecto_EDD/src/proyecto_edd/Prueba_Grafo.java
+++ Proyecto_EDD/src/proyecto_edd/Prueba_Grafo.java
@@ -1,12 +1,16 @@
 package proyecto_edd;
 
+import java.awt.Color;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
+import org.graphstream.ui.view.*;
 
 public class Prueba_Grafo {
     
     public static void main(String[] args) {
         Graph graph = new SingleGraph("Prueba");
+        
+        
         
         /*graph.addNode("A");
         graph.addNode("B");
@@ -15,12 +19,25 @@ public class Prueba_Grafo {
         graph.addEdge("BC", "B", "C");
         graph.addEdge("CA", "C", "A");*/
         
-        graph.setStrict(false);
+        /*graph.setStrict(false);
         graph.setAutoCreate(true);
         graph.addEdge("AB", "A", "B");
         graph.addEdge("BC", "B", "C");
         graph.addEdge("CA", "C", "A");
-        
+        */
         graph.display();
     }   
 }
+/*
+node {
+	fill-color: grey;
+	size: 10px;
+	stroke-mode: plain;
+	stroke-color: black;
+	stroke-width: 1px;
+}
+
+    node.important {
+	fill-color: red;
+	size: 15px;
+}*/
